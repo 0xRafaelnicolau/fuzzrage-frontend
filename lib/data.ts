2f852341bb85ee92233b8b92b971d1a2cc518d84
0xRafaelnicolau <0xrafaelnicolau@gmail.com>
2025-08-25 14:33:22 +0100

    ♻️  avoid querying the backend if theres no jwt present

--- lib/data.ts
+++ lib/data.ts
@@ -12,22 +12,24 @@ export async function getUser(): Promise<User | null> {
     const store = await cookies()
     const token = store.get('jwt')?.value
 
-    const response = await fetch(`${process.env.BACKEND_URL}/v1/user`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    })
+    if (token) {
+        const response = await fetch(`${process.env.BACKEND_URL}/v1/user`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
 
-    if (response.ok) {
-        const data = await response.json()
+        if (response.ok) {
+            const data = await response.json()
 
-        if (data?.data?.attributes) {
-            return {
-                name: data.data.attributes.name,
-                email: data.data.attributes.email,
-                avatar_url: data.data.attributes.avatar_url
+            if (data?.data?.attributes) {
+                return {
+                    name: data.data.attributes.name,
+                    email: data.data.attributes.email,
+                    avatar_url: data.data.attributes.avatar_url
+                }
             }
         }
     }
