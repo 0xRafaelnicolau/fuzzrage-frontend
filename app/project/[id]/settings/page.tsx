ab69d84bd5112cbfb888beccfa224c0ceec14c10
0xRafaelnicolau <0xrafaelnicolau@gmail.com>
2025-09-04 17:13:47 +0100

    ✨ introduce update project view

--- app/project/[id]/settings/page.tsx
+++ app/project/[id]/settings/page.tsx
@@ -1,7 +1,24 @@
-export default function Page() {
+import { UpdateProject } from "@/components/projects/settings/update-project";
+import { getProject } from "@/lib/actions/projects";
+import { notFound } from "next/navigation";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const { id } = await params;
+
+    const response = await getProject({ projectId: id });
+    if (!response.success || !response.project) {
+        notFound();
+    }
+
+    const project = response.project;
+
     return (
-        <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">General</h2>
-        </div>
+        <main>
+            <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">General</h2>
+            </div>
+
+            <UpdateProject project={project} />
+        </main>
     );
-}
+}   
--- components/projects/settings/config-list.tsx
+++ components/projects/settings/config-list.tsx
@@ -7,7 +7,7 @@ import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, } from "@/components/ui/cards/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import { deleteConfig, updateConfig } from "@/lib/actions/configs";
 import { toast } from "sonner";
