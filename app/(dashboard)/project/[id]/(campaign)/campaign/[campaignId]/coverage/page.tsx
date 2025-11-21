f4ae550a321356e3f3fddc415618765c446455d1
Rafael Nicolau <0xrafaelnicolau@gmail.com>
2025-11-21 00:00:18 +0000

    feat: do not display coverage report and properties if campaign not finished

--- app/(dashboard)/project/[id]/(campaign)/campaign/[campaignId]/coverage/page.tsx
+++ app/(dashboard)/project/[id]/(campaign)/campaign/[campaignId]/coverage/page.tsx
@@ -10,11 +10,47 @@ import { Folder, File, Tree } from "@/components/ui/file-tree";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Spinner } from "@/components/ui/spinner";
 import { ProgressCircle } from "@/components/ui/progress-circle";
+import { Campaign, GetCampaignRequest, getCampaign } from "@/lib/actions/campaigns";
+
+const options = [
+    { value: "SUCCEEDED", label: "Succeeded", color: "bg-green-500" },
+    { value: "FAILED", label: "Failed", color: "bg-red-500" },
+    { value: "RUNNING", label: "Running", color: "bg-orange-500" },
+    { value: "QUEUED", label: "Queued", color: "bg-yellow-500" },
+    { value: "CANCELLED", label: "Canceled", color: "bg-gray-400" },
+];
 
 export default function Page() {
     const params = useParams();
     const projectId = params.id as string;
     const campaignId = params.campaignId as string;
+
+
+    // Campaign
+    const [campaign, setCampaign] = useState<Campaign>();
+    const [loadingCampaign, setLoadingCampaign] = useState(false);
+
+    useEffect(() => {
+        const fetchCampaign = async () => {
+            setLoadingCampaign(true);
+
+            const request: GetCampaignRequest = {
+                project_id: projectId,
+                campaign_id: campaignId,
+            };
+
+            const result = await getCampaign(request);
+            if (result.success && result.campaign) {
+                setCampaign(result.campaign);
+            } else {
+                toast.error(result.error?.message || 'Failed to fetch campaign');
+            }
+
+            setLoadingCampaign(false);
+        }
+
+        fetchCampaign();
+    }, [projectId, campaignId]);
 
     // Tree
     const [tree, setTree] = useState<TreeNode[]>([]);
@@ -89,124 +125,157 @@ export default function Page() {
                     <p>{node.name}</p>
                 </File>
             )
-        )
+        );
 
     return (
         <>
-            <div className="p-3">
-                <div className="flex flex-col lg:flex-row relative">
-                    <div className="w-full lg:w-1/4 lg:pr-6 mb-6 lg:mb-0">
-                        <div className="lg:sticky lg:top-0">
-                            <div className="">
-                                <h2 className="text-lg font-semibold">Tree</h2>
-                            </div>
-                            <div className="bg-background border rounded-md mt-2">
-                                <div className="lg:hidden">
-                                    {loadingTree ? (
-                                        <div className="flex justify-center items-center py-16">
-                                            <Spinner variant="default" className="text-muted-foreground" />
-                                        </div>
-                                    ) : (
-                                        <Tree className="p-2 mt-1 overflow-auto" elements={tree}>
-                                            {renderTree(tree)}
-                                        </Tree>
-                                    )}
-                                </div>
-                                <ScrollArea className="hidden lg:block lg:h-[calc(100vh-16rem)]">
-                                    {loadingTree ? (
-                                        <div className="flex justify-center items-center py-16">
-                                            <Spinner variant="default" className="text-muted-foreground" />
-                                        </div>
-                                    ) : (
-                                        <Tree className="p-2 mt-1 overflow-auto" elements={tree}>
-                                            {renderTree(tree)}
-                                        </Tree>
-                                    )}
-                                </ScrollArea>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="w-full lg:w-3/4 lg:pl-4">
-                        <div className="flex items-center justify-between">
-                            <h2 className="text-lg font-semibold">Coverage</h2>
-                            {coverage?.percentage != null && (
-                                <ProgressCircle value={(coverage?.percentage || 0) * 100} className="size-7" />
-                            )}
-                        </div>
-                        <div className="bg-background border rounded-md mt-2">
-                            <div className="lg:hidden">
-                                {loadingCoverage ? (
-                                    <div className="flex justify-center items-center py-16">
-                                        <Spinner variant="default" className="text-muted-foreground" />
-                                    </div>
-                                ) : (
-                                    <div className="bg-background flex flex-col text-xs mt-2">
-                                        <style>{`
-                                        code { 
-                                            white-space: pre-wrap; 
-                                            display: block; 
-                                            background-color: bg-background; 
-                                        }
-                                        .executed { 
-                                            background-color: #306B34; 
-                                        }
-                                        .reverted { 
-                                            background-color: #FFD100; 
-                                        }
-                                        .unexecuted { 
-                                            background-color: #8B0000; 
-                                        }
-                                        .neutral { 
-                                            background-color: bg-background; 
-                                        }
-                                    `}</style>
-                                        {coverage?.content ? (
-                                            <div dangerouslySetInnerHTML={{ __html: coverage?.content }}></div>
-                                        ) : (
-                                            <div className="flex justify-center items-center py-16 text-muted-foreground text-base">No file selected.</div>
-                                        )}
-                                    </div>
-                                )}
-                            </div>
-                            <ScrollArea className="hidden lg:block lg:h-[calc(100vh-16rem)]">
-                                {loadingCoverage ? (
-                                    <div className="flex justify-center items-center py-16">
-                                        <Spinner variant="default" className="text-muted-foreground" />
-                                    </div>
-                                ) : (
-                                    <div className="bg-background flex flex-col text-xs mt-2">
-                                        <style>{`
-                                        code { 
-                                            white-space: pre-wrap; 
-                                            display: block; 
-                                            background-color: bg-background; 
-                                        }
-                                        .executed { 
-                                            background-color: #306B34; 
-                                        }
-                                        .reverted { 
-                                            background-color: #FFD100; 
-                                        }
-                                        .unexecuted { 
-                                            background-color: #8B0000; 
-                                        }
-                                        .neutral { 
-                                            background-color: bg-background; 
-                                        }
-                                    `}</style>
-                                        {coverage?.content ? (
-                                            <div dangerouslySetInnerHTML={{ __html: coverage?.content }}></div>
-                                        ) : (
-                                            <div className="flex justify-center items-center py-16 text-muted-foreground text-base">No file selected.</div>
-                                        )}
-                                    </div>
-                                )}
-                            </ScrollArea>
-                        </div>
-                    </div>
+            {loadingCampaign ? (
+                <div className="flex justify-center items-center py-16">
+                    <Spinner variant="default" className="text-muted-foreground" />
                 </div>
-            </div >
+            ) : (
+                <>
+                    {campaign && (
+                        <>
+                            {(campaign.attributes.state === "SUCCEEDED" ||
+                                campaign.attributes.state === "FAILED" ||
+                                campaign.attributes.state === "CANCELLED") && (
+                                    <div className="p-3">
+                                        <div className="flex flex-col lg:flex-row relative">
+                                            <div className="w-full lg:w-1/4 lg:pr-6 mb-6 lg:mb-0">
+                                                <div className="lg:sticky lg:top-0">
+                                                    <div className="">
+                                                        <h2 className="text-lg font-semibold">Tree</h2>
+                                                    </div>
+                                                    <div className="bg-background border rounded-md mt-2">
+                                                        <div className="lg:hidden">
+                                                            {loadingTree ? (
+                                                                <div className="flex justify-center items-center py-16">
+                                                                    <Spinner variant="default" className="text-muted-foreground" />
+                                                                </div>
+                                                            ) : (
+                                                                <Tree className="p-2 mt-1 overflow-auto" elements={tree}>
+                                                                    {renderTree(tree)}
+                                                                </Tree>
+                                                            )}
+                                                        </div>
+                                                        <ScrollArea className="hidden lg:block lg:h-[calc(100vh-16rem)]">
+                                                            {loadingTree ? (
+                                                                <div className="flex justify-center items-center py-16">
+                                                                    <Spinner variant="default" className="text-muted-foreground" />
+                                                                </div>
+                                                            ) : (
+                                                                <Tree className="p-2 mt-1 overflow-auto" elements={tree}>
+                                                                    {renderTree(tree)}
+                                                                </Tree>
+                                                            )}
+                                                        </ScrollArea>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="w-full lg:w-3/4 lg:pl-4">
+                                                <div className="flex items-center justify-between">
+                                                    <h2 className="text-lg font-semibold">Coverage</h2>
+                                                    {coverage?.percentage != null && (
+                                                        <ProgressCircle value={(coverage?.percentage || 0) * 100} className="size-7" />
+                                                    )}
+                                                </div>
+                                                <div className="bg-background border rounded-md mt-2">
+                                                    <div className="lg:hidden">
+                                                        {loadingCoverage ? (
+                                                            <div className="flex justify-center items-center py-16">
+                                                                <Spinner variant="default" className="text-muted-foreground" />
+                                                            </div>
+                                                        ) : (
+                                                            <div className="bg-background flex flex-col text-xs mt-2">
+                                                                <style>{`
+                                        code { 
+                                            white-space: pre-wrap; 
+                                            display: block; 
+                                            background-color: bg-background; 
+                                        }
+                                        .executed { 
+                                            background-color: #306B34; 
+                                        }
+                                        .reverted { 
+                                            background-color: #FFD100; 
+                                        }
+                                        .unexecuted { 
+                                            background-color: #8B0000; 
+                                        }
+                                        .neutral { 
+                                            background-color: bg-background; 
+                                        }
+                                    `}</style>
+                                                                {coverage?.content ? (
+                                                                    <div dangerouslySetInnerHTML={{ __html: coverage?.content }}></div>
+                                                                ) : (
+                                                                    <div className="flex justify-center items-center py-16 text-muted-foreground text-base">No file selected.</div>
+                                                                )}
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                    <ScrollArea className="hidden lg:block lg:h-[calc(100vh-16rem)]">
+                                                        {loadingCoverage ? (
+                                                            <div className="flex justify-center items-center py-16">
+                                                                <Spinner variant="default" className="text-muted-foreground" />
+                                                            </div>
+                                                        ) : (
+                                                            <div className="bg-background flex flex-col text-xs mt-2">
+                                                                <style>{`
+                                        code { 
+                                            white-space: pre-wrap; 
+                                            display: block; 
+                                            background-color: bg-background; 
+                                        }
+                                        .executed { 
+                                            background-color: #306B34; 
+                                        }
+                                        .reverted { 
+                                            background-color: #FFD100; 
+                                        }
+                                        .unexecuted { 
+                                            background-color: #8B0000; 
+                                        }
+                                        .neutral { 
+                                            background-color: bg-background; 
+                                        }
+                                    `}</style>
+                                                                {coverage?.content ? (
+                                                                    <div dangerouslySetInnerHTML={{ __html: coverage?.content }}></div>
+                                                                ) : (
+                                                                    <div className="flex justify-center items-center py-16 text-muted-foreground text-base">No file selected.</div>
+                                                                )}
+                                                            </div>
+                                                        )}
+                                                    </ScrollArea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div >
+                                )}
+
+                            {(campaign.attributes.state === "RUNNING" ||
+                                campaign.attributes.state === "QUEUED") && (
+                                    <div className="p-3">
+                                        <div className="flex flex-col items-center justify-center py-16 text-center">
+                                            <div className="flex items-center gap-2">
+                                                <h1 className="text-2xl font-bold">
+                                                    {options.find(opt => opt.value === campaign.attributes.state)?.label ??
+                                                        campaign.attributes.state}
+                                                </h1>
+                                            </div>
+                                            <p className="text-sm mt-1 text-muted-foreground">
+                                                Properties will be available once the campaign has finished.
+                                            </p>
+                                        </div>
+                                    </div>
+                                )}
+                        </>
+                    )}
+                </>
+            )}
         </>
-    )
+    );
 }
--- app/(dashboard)/project/[id]/(campaign)/campaign/[campaignId]/properties/page.tsx
+++ app/(dashboard)/project/[id]/(campaign)/campaign/[campaignId]/properties/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { GetCampaignRequest, getCampaign } from "@/lib/actions/campaigns";
+import { Campaign, GetCampaignRequest, getCampaign } from "@/lib/actions/campaigns";
 import { toast } from "sonner";
 import { Spinner } from "@/components/ui/spinner";
 import PropertiesCards from "@/app/(dashboard)/project/[id]/(campaign)/campaign/[campaignId]/properties/properties-cards";
@@ -12,18 +12,28 @@ export type Property = {
     status: "Passed" | "Failed"
 }
 
+const options = [
+    { value: "SUCCEEDED", label: "Succeeded", color: "bg-green-500" },
+    { value: "FAILED", label: "Failed", color: "bg-red-500" },
+    { value: "RUNNING", label: "Running", color: "bg-orange-500" },
+    { value: "QUEUED", label: "Queued", color: "bg-yellow-500" },
+    { value: "CANCELLED", label: "Canceled", color: "bg-gray-400" },
+];
+
 export default function Page() {
     const params = useParams();
     const projectId = params.id as string;
     const campaignId = params.campaignId as string;
 
+    const [campaign, setCampaign] = useState<Campaign>();
+    const [loading, setLoading] = useState(true);
+
     // Properties
     const [properties, setProperties] = useState<Property[]>([]);
-    const [loadingProperties, setLoadingProperties] = useState(true);
 
     useEffect(() => {
         const fetchProperties = async () => {
-            setLoadingProperties(true);
+            setLoading(true);
 
             const request: GetCampaignRequest = {
                 project_id: projectId,
@@ -32,6 +42,8 @@ export default function Page() {
 
             const result = await getCampaign(request);
             if (result.success && result.campaign) {
+                setCampaign(result.campaign);
+
                 const failedProperties = result.campaign.attributes.result.props_failed.split(';');
                 const passedProperties = result.campaign.attributes.result.props_passed.split(';');
 
@@ -50,7 +62,7 @@ export default function Page() {
                 toast.error(result.error?.message || 'Failed to fetch properties');
             }
 
-            setLoadingProperties(false);
+            setLoading(false);
         }
         fetchProperties();
     }, [projectId, campaignId])
@@ -58,12 +70,41 @@ export default function Page() {
     return (
         <main>
             <div className="h-full flex flex-col p-3">
-                {loadingProperties ? (
+                {loading ? (
                     <div className="flex justify-center items-center py-16">
                         <Spinner variant="default" className="text-muted-foreground" />
                     </div>
                 ) : (
-                    <PropertiesCards projectId={projectId} campaignId={campaignId} properties={properties} />
+                    <>
+                        {campaign && (
+                            <>
+                                {(campaign.attributes.state === "SUCCEEDED" ||
+                                    campaign.attributes.state === "FAILED" ||
+                                    campaign.attributes.state === "CANCELLED") && (
+                                        <PropertiesCards
+                                            projectId={projectId}
+                                            campaignId={campaignId}
+                                            properties={properties}
+                                        />
+                                    )}
+
+                                {(campaign.attributes.state === "RUNNING" ||
+                                    campaign.attributes.state === "QUEUED") && (
+                                        <div className="flex flex-col items-center justify-center py-16 text-center">
+                                            <div className="flex items-center gap-2">
+                                                <h1 className="text-2xl font-bold">
+                                                    {options.find(opt => opt.value === campaign.attributes.state)?.label ??
+                                                        campaign.attributes.state}
+                                                </h1>
+                                            </div>
+                                            <p className="text-sm mt-1 text-muted-foreground">
+                                                Properties will be available once the campaign has finished.
+                                            </p>
+                                        </div>
+                                    )}
+                            </>
+                        )}
+                    </>
                 )}
             </div>
         </main >
